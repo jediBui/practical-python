ce02d7e1a0733a169c91682dd935d27367f9f340
Vu Bui <vu@thebu.is>
2023-07-31 23:26:26 -0500

    day 1

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+Height = 100 # 100 meters
+#rebound = .60 # 3/5 = 60% = .6
+#bounceLimit = 10
+bounceCount = 1
+
+while bounceCount <= 10:
+    Height = (Height * 3/5)
+    print (bounceCount, round(Height, 1))
+    bounceCount += 1
